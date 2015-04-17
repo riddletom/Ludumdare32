20b6957dfa23d53e2ac7c9ac416462dd73975945
GuiBruche <dunin@laposte.net>
2015-04-17 22:55:42 +0200

    my bad

--- js/Boot.js
+++ js/Boot.js
@@ -33,7 +33,7 @@ BasicGame.Boot.prototype = {
         //  By this point the preloader assets have loaded to the cache, we've set the game settings
         //  So now let's start the real preloader going
         //this.state.start('Preloader');
-        this.state.start('Game');
+        this.state.start('Preloader');
     }
 
 };
