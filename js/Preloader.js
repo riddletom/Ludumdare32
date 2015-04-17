583ae01364b166deda5fb4ea89c036b09f30449f
GuiBruche <dunin@laposte.net>
2015-04-17 22:34:42 +0200

    DANS PRELOAD!!

--- js/Preloader.js
+++ js/Preloader.js
@@ -11,7 +11,7 @@ BasicGame.Preloader = function (game) {
 BasicGame.Preloader.prototype = {
 
 	preload: function () {
-		
+		/*
 		//	These are the assets we loaded in Boot.js
 		//	A nice sparkly background and a loading progress bar
 		this.background = this.add.sprite(0, 0, 'preloaderBackground');
@@ -29,7 +29,7 @@ BasicGame.Preloader.prototype = {
 		this.load.audio('titleMusic', ['audio/main_menu.mp3']);
 		this.load.bitmapFont('caslon', 'fonts/caslon.png', 'fonts/caslon.xml');
 		//	+ lots of other required assets here
-		
+		*/
 
 	},
 
