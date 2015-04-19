e7460a2a7f7e726962e607b942aa2db4243c7a6b
riddletom <thomas.fabre-mary@wanadoo.fr>
2015-04-20 00:11:09 +0200

    Push last code of the day

--- js/Game.js
+++ js/Game.js
@@ -29,105 +29,221 @@ BasicGame.Game.prototype = {
 
     create: function () {
         console.log("lancement du jeu");
-        //test parquet 
-        for (var i=0;i<20;i++) {
-            for (var j=0;j<20;j++) {
-                var parquet = this.game.add.sprite(i*30, j*30, 'parquet');
 
-            }
-        }
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        //test murs
-        for (var i=0;i<20;i++) { 
-            var mur = this.game.add.sprite(15+i*30, 15, 'mur');
-            mur.anchor.setTo(0.5,0.5);
-            var mur2 = this.game.add.sprite(15+i*30, 15+570, 'mur');
-            mur2.anchor.setTo(0.5,0.5);
-            mur2.rotation = Math.PI;
-            if (i==0 || i==19) {
-                mur.animations.add('angle',[1]);
-                mur.play('angle');
-                mur2.animations.add('angle',[1]);
-                mur2.play('angle');
-                mur2.rotation = Math.PI/2;
-                if(i==0) {
-                    mur.rotation = Math.PI /2*3;
-                    mur2.rotation = Math.PI;
-                }
-            } else {
-                // côtés
-                var mur3 = this.game.add.sprite(15, 15+i*30, 'mur');
-                var mur4 = this.game.add.sprite(585, 15+i*30, 'mur');
-                mur3.anchor.setTo(0.5,0.5);
-                mur4.anchor.setTo(0.5,0.5);
-                mur3.rotation = Math.PI/2*3;
-                mur4.rotation = Math.PI/2;
-            }
-        }
 
-        // test carpet
-        this.carpets=[];
-        this.compteur=0;
-        for (var i=0;i<10;i++) {
-            var carpet = this.game.add.sprite(108, 100+i*30, 'carpet');
-            carpet.animations.add('normal',[0]);
-            carpet.animations.add('vague',[6]);
-            carpet.play('normal')
-            //carpet.scale.setTo(2,2);
-            //carpet.smoothed = false;
-            this.carpets.push(carpet);
-        }
-        for (var i=0;i<10;i++) {
-            var carpet = this.game.add.sprite(408, 100+i*30, 'carpet');
-            carpet.animations.add('normal',[0]);
-            carpet.play('normal')
-            //carpet.scale.setTo(2,2);
-            //carpet.smoothed = false;
-        }
+        //create map
+        map = this.game.add.tilemap('map');
+        map.addTilesetImage('mur');
+        map.addTilesetImage('carrelage');
+        tilesetcarpet= map.addTilesetImage('carpet1');
+        
+        //create layer
+        solLayer = map.createLayer('sol');
+        murLayer = map.createLayer('mur');
+        tapisLayer = map.createLayer('tapis');
 
-        this.time.events.loop(Phaser.Timer.SECOND/8 , function() {
-            this.carpets[this.compteur].play('vague');
-            if (this.compteur==0) {
-                this.carpets[this.carpets.length-1].play('normal');
-            } else this.carpets[this.compteur-1].play('normal');
-            this.compteur++;
-            if (this.compteur==this.carpets.length) this.compteur=0;
-        }, this);
-       
-       //  test animations player
-        for (var i=0;i<4;i++) {
-             var player = this.game.add.sprite(100+100*i, 200, 'player');
-             player.animations.add('face',[0,1,2,3,4,5,4,3,2,1]);
-             player.animations.add('dos',[6,7,8,9,10,11,10,9,8,7]);
-             player.animations.add('profil',[12,13,14,15,16,17]);
-             player.animations.add('dead',[18,19,20,21]);
+        //collision on blockedLayer
+        map.setCollisionBetween(1, 200, true, 'mur');
+        
+        //create player
+        player = this.game.add.sprite(260, 200, 'player');
+        player.anchor.set(0.5,0.5);
+        player.direction=6;
+        player.countdown=0;
 
-             switch (i) {
-                case 0:
-                     player.animations.play('face', 20, true);
-                     break;
-                case 1:
-                    player.animations.play('profil', 15, true);
-                    break;
-                case 2:
-                    player.animations.play('dos', 20, true);
-                    break;
-                case 3:
-                    player.animations.play('dead', 20, true);
-                    break;
-                default:
-             }
-             player.scale.setTo(1.5,1.5);
-             player.smoothed = false
-        }
+             //player animations
+            player.animations.add('face',[0,1,2,3,4,5,4,3,2,1]);
+            player.animations.add('dos',[6,7,8,9,10,11,10,9,8,7]);
+            player.animations.add('profil',[12,13,14,15,16,17]);
+            player.animations.add('dead',[18,19,20,21]);
+        
+            //player physics
+            this.game.physics.enable(player);
+            player.body.bounce.x = 0;
+            player.body.bounce.y = 0;
+            player.body.setSize(12,6,0,8);
+
+        //essai adversaire
+        player2 = this.game.add.sprite(280, 400, 'player');
+        player2.anchor.set(0.5,0.5);
+        this.game.physics.enable(player2);
+        player2.animations.add('dead',[18,19,20,21]);
+        player2.body.setSize(12,6,0,8);
+        //  Our controls.
+        cursors = this.game.input.keyboard.createCursorKeys();
 
     },
 
     update: function () {
+    var stop =false;
+            //collision 
+    this.game.physics.arcade.collide(player,murLayer,function(){
+
+
+        switch (player.direction) {
+                case 0:
+                     player.frame = 9;
+                     break;
+                case 3:
+                    player.frame = 16;
+                    break;
+                case 6:
+                    player.frame = 3;
+                    break;
+                case 9:
+                    player.frame = 16;
+                    break;
+                default:
+             }
+        player.animations.stop();
+        stop=true;
 
 
 
+    },null,this);
 
+    //player management
+    player.body.velocity.x = 0;
+    player.body.velocity.y = 0;   
+
+    if (cursors.left.isDown)
+    {
+        //  Move to the left
+        player.direction=9; //heure sur une horloge
+        player.body.velocity.x = -150;
+        if (!stop) player.animations.play('profil');
+        if (player.scale.x < 0)
+            {player.scale.x*=-1;}
+
+    }
+    else if (cursors.right.isDown)
+    {
+        //  Move to the right
+        player.direction=3;
+        player.body.velocity.x = 150;
+        if (player.scale.x>0)
+           {player.scale.x*=-1;}
+
+        if (!stop) player.animations.play('profil');
+
+    }
+
+    if (cursors.down.isDown)
+    {
+        //  Move down
+        player.direction=6;
+        player.body.velocity.y = 150;
+        if (!stop) player.animations.play('face');
+    }
+    else if (cursors.up.isDown)
+    {
+        //  Move up
+        player.direction=0;
+        player.body.velocity.y = -150;
+
+        if (!stop) player.animations.play('dos');
+    }
+    
+
+    //procédure de lancement de tapis avec barre espace
+    if(this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).isDown && player.countdown==0)
+    {
+        var tilex = solLayer.getTileX(player.body.x);
+        var tiley = solLayer.getTileY(player.body.y);
+        var tileID = tilesetcarpet.firstgid;
+        switch (player.direction) {
+                case 0:
+                     tiley--;
+                    tileID+=4;
+                     break;
+                case 3:
+                    tilex++;
+                    tileID+=5;
+                    break;
+                case 6:
+                    tiley++;
+                    tileID+=2;
+                    break;
+                case 9:
+                    tilex--;
+                    tileID+=3;
+                    break;
+                default:
+             }
+             //console.log("tileID cherché="+tileID+" tile ID Réel"+map.getTile(tilex,tiley,tapisLayer,true).index);
+
+           if(map.getTile(tilex,tiley,tapisLayer,true).index == tileID)
+             {  map.swap(tileID,tileID+6,tilex,tiley,1,1,tapisLayer);
+                carpetarray.push({"tilex":tilex,"tiley":tiley,"direction":player.direction,"count":framerate})          
+                player.countdown=framerate*6;
+            }
+
+    }
+    else if (player.countdown>0)
+    { 
+      player.countdown--;
+    }
+
+    // avancée tapis
+    for (var i=carpetarray.length-1;i>-1;i--)
+        {
+            if(carpetarray[i].count==0)
+             {
+              var ID= map.getTile(carpetarray[i].tilex,carpetarray[i].tiley,tapisLayer,true).index
+              map.swap(ID,ID-6,carpetarray[i].tilex,carpetarray[i].tiley,1,1,tapisLayer);
+                switch (carpetarray[i].direction) 
+                {
+                    case 0:
+                         carpetarray[i].tiley--;
+                        break;
+                    case 3:
+                        carpetarray[i].tilex++;
+                        break;
+                    case 6:
+                        carpetarray[i].tiley++;
+                        break;
+                    case 9:
+                        carpetarray[i].tilex--;
+                        break;
+                default:
+                }
+                ID= map.getTile(carpetarray[i].tilex,carpetarray[i].tiley,tapisLayer,true).index
+                if(ID<0||(ID+carpetarray[i].direction)%2==0)
+                {
+                    carpetarray.splice(i,1);
+                }
+                else
+                {
+                    map.swap(ID,ID+6,carpetarray[i].tilex,carpetarray[i].tiley,1,1,tapisLayer);
+                    carpetarray[i].count=framerate;
+                }    
+              }
+            else
+            {
+             carpetarray[i].count--;
+
+             //console.log("rentre dans la boucle "+i+ " reste framerate:"+carpetarray[i].count);
+            }
+            //carpet.scale.setTo(2,2);
+            //carpet.smoothed = false;
+        }
+
+    //collision tapis
+    console.log(map.getTile(solLayer.getTileX(player2.body.x),solLayer.getTileY(player2.body.y),tapisLayer,true).index)
+    if(map.getTile(solLayer.getTileX(player2.body.x),solLayer.getTileY(player2.body.y),tapisLayer,true).index>(tilesetcarpet.firstgid+6-1))
+
+    {
+        player2.animations.play('dead');
+
+    }
+
+    },
+    render: function (pointer){
+
+    this.game.debug.body(player);
+   this.game.debug.body(player2);
 
     },
 
